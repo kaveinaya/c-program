a4c6ddffa5dc495c8dcff5570572ba7398fc9181
kaveinaya <34646340+kaveinaya@users.noreply.github.com>
2017-12-19 12:08:28 +0530

    Create variables.c

---
+++ beginner/variables.c
@@ -0,0 +1,14 @@
+
+int main()
+{
+char a[30]="Sachin Ramesh Tendulkar";
+char b[20]="Little Master";
+int c=18426 ,d= 463;
+float e=44.83;
+printf("%c",a);
+printf("%c",b);
+printf("%d",c);
+printf("%f",e);
+printf("%d",d);
+return 0;
+}
