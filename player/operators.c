b2d044cf994d9db3ef0618a71378b86d80005723
kaveinaya <34646340+kaveinaya@users.noreply.github.com>
2017-12-19 12:11:55 +0530

    Create operators.c

---
+++ player/operators.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a=5,b=5;
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+g=a%b;
+printf("%d",c);
+printf("%d",d);
+printf("%d",e);
+printf("%d",f);
+printf("%d",g);
+return 0;
+}
+
