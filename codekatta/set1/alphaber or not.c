682d455b9d0812183c3108289d26f84652d48f5a
kaveinaya <34646340+kaveinaya@users.noreply.github.com>
2017-12-19 13:01:17 +0530

    Update alphaber or not.c

--- codekatta/set1/alphaber or not.c
+++ codekatta/set1/alphaber or not.c
@@ -3,7 +3,7 @@ int main(void)
 {
        char z;
        scanf("%c",&z);
-       if((z>=97&z<=122)||(z>=65&z<=90))
+       if((z>=97&&z<=122)||(z>=65&&z<=90))
        {
        	printf("alphabet");
        }
